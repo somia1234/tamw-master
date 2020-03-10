3c89f04720a41d2766d199817ba9795404a54b90
somia1234 <soma_wageih@yahoo.com>
2020-03-10 20:18:22 +0200

    somia commit

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:tamwink/Animation/FadeAnimation.dart';
+import 'package:flutter/material.dart';
+import 'package:tamwink/maincustomer.dart';
+
+void main() => runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Directionality( textDirection: TextDirection.rtl,
+        child: HomePage(),
+      ),
+    )
+);
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                colors: [
+                  Colors.orange[900],
+                  Colors.orange[800],
+                  Colors.orange[400]
+                ]
+            )
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 60,),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                  FadeAnimation(1, Text("تموينك", style: TextStyle(color: Colors.white, fontSize: 40),)),
+              SizedBox(height: 10,),
+              FadeAnimation(1.3, Text("مرحبا بكم", style: TextStyle(color: Colors.white, fontSize: 15,))),
+                ],
+              ),
+
+            ),
+            SizedBox(height: 15,),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60))
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 60,),
+                      FadeAnimation(1.4, Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [BoxShadow(
+                                color: Color.fromRGBO(225, 95, 27, .3),
+                                blurRadius: 20,
+                                offset: Offset(0, 10)
+                            )]
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  border: Border(bottom: BorderSide(color: Colors.grey[200]))
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: "رقم الهاتف",
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                    border: InputBorder.none
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  border: Border(bottom: BorderSide(color: Colors.grey[200]))
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: "كلمة المرور",
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                    border: InputBorder.none
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )),
+                      SizedBox(height: 35,),
+                      FadeAnimation(1.5, Text("نسيت كلمة المرور؟", style: TextStyle(color: Colors.grey),)),
+                      SizedBox(height: 40,),
+                      FadeAnimation(1.6, Container(
+                        height: 40,
+                        margin: EdgeInsets.symmetric(horizontal: 50),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.orange[900]
+                        ),
+                        child: Center(
+
+                          child:
+                            FlatButton(
+                              onPressed: () {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => MyHomePage()
+                                ));
+                              },
+                               child:
+                               Text("تسجيل الدخول", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+
+
+                            ),
+
+
+                        ),
+                      )
+                      ),
+
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
